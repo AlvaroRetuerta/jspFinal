d5bc86982dad1b30afe559a0d8132b4a96368620
ALUM15 <ALUM15@PC15>
2018-01-29 13:51:34 +0100

    a

---
+++ webjsp/src/main/java/es/altair/controller/AnadirVehiculo.java
@@ -0,0 +1,90 @@
+package es.altair.controller;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
+
+import es.altair.bean.Vehiculo;
+import es.altair.dao.VehiculoDAO;
+import es.altair.dao.VehiculoDAOImplHibernate;
+
+/**
+ * Servlet implementation class AnadirVehiculo
+ */
+public class AnadirVehiculo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AnadirVehiculo() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String matricula =request.getParameter("matricula");
+		String marca=request.getParameter("marca");
+		String modelo=request.getParameter("modelo");
+		String pais=request.getParameter("pais");
+		int anyo=Integer.parseInt(request.getParameter("anyo"));
+		
+		//tratamiento de la imagen
+		Part filePart = request.getPart("imagen");
+		
+		InputStream inputS = null;
+		ByteArrayOutputStream os=null;
+		if(!getFileName(filePart).equals("")) {
+			inputS = filePart.getInputStream();
+			
+			//escalar imagen
+			BufferedImage imageBuffer = ImageIO.read(inputS);
+			Image tmp = imageBuffer.getScaledInstance(640, 640, BufferedImage.SCALE_FAST);
+			BufferedImage buffered = new BufferedImage(640, 640, BufferedImage.TYPE_INT_RGB);
+			buffered.getGraphics().drawImage(tmp, 0, 0, null);
+			
+			os=new ByteArrayOutputStream();
+			ImageIO.write(buffered, "jpg", os);
+		}
+		HttpSession sesion = request.getSession();
+		
+		VehiculoDAO vDAO = new VehiculoDAOImplHibernate();
+
+		Vehiculo v = new Vehiculo(matricula, marca, modelo, anyo, pais);
+		
+		vDAO.insertar(v);
+		
+		
+		response.sendRedirect("jsp/principalUsu");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	private String getFileName(Part filePart) {
+		for (String content : filePart.getHeader("content-disposition").split(";")) {
+			if(content.trim().startsWith("filename")) {
+				return content.substring(content.indexOf("=")+1).trim().replace("\"","");
+			}
+		}
+		return null;
+	}
+
+}
---
+++ webjsp/src/main/webapp/jsp/principalUsuario.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<link rel="stylesheet" href="../css/bootstrap.min.css">
+<link rel="stylesheet" href="../css/css2.css">
+
+<!-- Stylesheets -->
+<link rel="stylesheet" href="../fonts/font-awesome.min.css">
+<title>Insert title here</title>
+</head>
+<body>
+
+
+
+
+<script src="../js/jquery-3.2.1.slim.min.js"></script>
+	<script src="../js/popper.min.js"></script>
+	<script src="../js/bootstrap.min.js"></script>
+</body>
+</html>
