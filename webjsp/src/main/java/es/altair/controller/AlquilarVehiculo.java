16ea4530b69acf9d9a7294cab27041168da1fd44
Alvaro <Alvaro@192.168.1.116>
2018-01-31 21:27:16 +0100

    1

--- webjsp/src/main/java/es/altair/controller/AlquilarVehiculo.java
+++ webjsp/src/main/java/es/altair/controller/AlquilarVehiculo.java
@@ -1,10 +1,22 @@
 package es.altair.controller;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import es.altair.bean.Alquiler;
+import es.altair.bean.Vehiculo;
+import es.altair.dao.AlquilerDAO;
+import es.altair.dao.AlquilerDAOImplHibernate;
+import es.altair.dao.VehiculoDAO;
+import es.altair.dao.VehiculoDAOImplHibernate;
 
 /**
  * Servlet implementation class AlquilarVehiculo
@@ -25,15 +37,38 @@ public class AlquilarVehiculo extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	
+		String matricula = request.getParameter("matricula");
+		VehiculoDAO vDAO = new VehiculoDAOImplHibernate();
+		Vehiculo v = vDAO.obtener(matricula);
+		AlquilerDAO aDAO = new AlquilerDAOImplHibernate();
+		List<Alquiler> alquileres = aDAO.alquileresActuales();
+		
+		
+		
+		for (Alquiler a : alquileres) {
+			
+			if(a.getVehiculo().getId()==v.getId()) {
+				response.sendRedirect("jsp/principalUsuario?mensaje=El vehiculo ya esta alquilado");
+			}
+			
+		}
+		Date currentDate = new Date();
+		Calendar c = Calendar.getInstance();
+        c.setTime(currentDate);
+        c.add(Calendar.DATE, 7);
+        
+		//aDAO.save(new Alquiler(user, v , currentDate, c.getTime())); //terminar con la fecha y el usuario
+		response.sendRedirect("jsp/principalUsuario?mensaje=El alquiler expirará en una semana");
+		
+		
 	}
 
 }
